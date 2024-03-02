c9742a31ea038eb03a0bc45a429ae845c2f93d6d
AMAR <amar.darekar.1991@gmail.com>
2024-03-02 21:53:07 +0530

    Initial commit

---
+++ src/test/java/com/learnwithamar/ats/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithamar.ats.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
